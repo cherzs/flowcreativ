26e2fad202e07f5b63295d463fda705fe05eff49
cherzs <zhafrang638@gmail.com>
2025-12-16 01:34:30 +0700

    Increase logo size in header

--- components/header.tsx
+++ components/header.tsx
@@ -26,9 +26,9 @@ export default function Header() {
           <Image
             src="/logo.png"
             alt="FlowCreativ Logo"
-            width={150}
-            height={40}
-            className="h-10 w-auto object-contain"
+            width={380}
+            height={128}
+            className="h-16 w-auto object-contain"
             priority
           />
         </Link>
